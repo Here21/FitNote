d2358c99c405e646011e19cbf3a00a6a97cf9a47
Here21 <deletetmr@gmail.com>
2019-01-09 22:12:29 +0800

    [UI] 折叠面板

---
+++ src/components/ExpansionPanel/ExpansionPanel.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const styles = theme => ({
+  root: {
+    width: '100%'
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular
+  }
+});
+
+function SimpleExpansionPanel(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <ExpansionPanel onChange={handleChange}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>Expansion Panel 1</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>Expansion Panel 2</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>
+            Disabled Expansion Panel
+          </Typography>
+        </ExpansionPanelSummary>
+      </ExpansionPanel>
+    </div>
+  );
+}
+const handleChange = ev => {
+  console.log(ev);
+};
+
+SimpleExpansionPanel.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SimpleExpansionPanel);
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import TrainingService from '../../service/TrainingService';
+import ExpansionPanel from '../../components/ExpansionPanel';
 
 class HomePage extends Component {
+  componentDidMount() {
+    TrainingService.getHistory().then(res => {
+      console.log(res);
+    });
+  }
+
   render() {
     return (
       <div>
+        <ExpansionPanel />
         <Button variant="contained">
           <Link to={'/actions'}>Link</Link>
         </Button>
